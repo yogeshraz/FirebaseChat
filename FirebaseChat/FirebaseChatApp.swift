df034f7a9e79c5e9c59d4b5402db399d65c4f64f
Yogesh Raj <yogeshraz@hotmail.com>
2025-04-21 11:37:26 +0530

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by Yogesh Raj on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
